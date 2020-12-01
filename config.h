6e3e6c54f44504a317ded5846c446c0cb571387a
imrufbd <imrufbd@gmail.com>
2020-12-01 19:51:52 +0600

    suckless slock build

---
+++ config.h
@@ -0,0 +1,25 @@
+/* user and group to drop privileges to */
+static const char *user  = "masud";
+static const char *group = "users";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+static const char * message = "Type Password To Unlock:";
+static const char * text_color = "#ffffff";
+static const char * font_name = "lucidasanstypewriter-14";
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
